1e7a04e7c71a073adb46ce281a9dfa72aaceca5c
taroooth <okada@dptr.jp>
2021-07-25 14:32:21 +0900

    Chapter6 エラーページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,16 @@
 class ApplicationController < ActionController::Base
     layout :set_layout
 
-    resque_from StandardError, with: :resque500
+    class Forbidden < ActionController::ActionControllerError; end
+    class IpAddressRejected < ActionController::ActionControllerError; end
+
+    include ErrorHandlers if Rails.env.production?
 
     private def set_layout
-        if params[:controller].match(%r{\A(staff|admin|customer)/})
-            Regexp.last_match[1]
-        else
-            "customer"
-        end
-    end
-
-    private def resque500(e)
-        render "errors/internal_server_error", status: 500
+      if params[:controller].match(%r{\A(staff|admin|customer)/})
+          Regexp.last_match[1]
+      else
+          "customer"
+      end
     end
 end
