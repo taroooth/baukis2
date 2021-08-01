bfd95ec95a9ba489bb2762dad70aa72e49d4e038
taroooth <okada@dptr.jp>
2021-08-01 16:04:09 +0900

    Chapter9 ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,33 @@
 Rails.application.routes.draw do
-  namespace :staff do
-    root "top#index"
+  config = Rails.application.config.baukis2
 
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      post "session" => "sessions#create", as: :session
+      delete "session" => "sessions#destroy"
+      # エラーになる(ActionController::RoutingError (No route matches [DELETE] "/session"):)
+      # resource :session, only: [ :create, :destory ]
+      resource :account, except: [ :new, :create, :destroy ]
+    end
   end
 
-  namespace :admin do
-    root "top#index"
-    
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      post "session" => "sessions#create", as: :session
+      delete "session" => "sessions#destroy"
+      # エラーになる(ActionController::RoutingError (No route matches [DELETE] "/session"):)
+      # resource :session, only: [ :create, :destory ]
+      resources :staff_members
+    end
   end
 
-  namespace :customer do
-    root "top#index"
+  constraints host: config[:customer][:host] do
+    namespace :customer, path: config[:customer][:path] do
+      root "top#index"
+  end
   end
 end
